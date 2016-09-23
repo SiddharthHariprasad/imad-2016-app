21e9c39cfb887930bf3d5637ada93f153d11f625
SiddharthHariprasad <siddhuhariprasad@gmail.com>
2016-09-23 19:36:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -633,7 +633,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-
 app.get('/ui/Sid.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Sid.png'));
 });
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+
+//change text
+
+var element = document.getElementById ("main-text");
+
+element.innerHTML = "Welcome to the Harry Potter Website!";
+
+//move logo
+
+var img = document.getElementById("logo");
+var marginLeft = 0;
+function moveRight () {
+		marginLeft = marginLeft + 1;
+		img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function() {
+	var interval = setInterval(moveRight,50)
+}
