f41174258fb689cdf2fb5e8a2693889dd1e969b5
SiddharthHariprasad <siddhuhariprasad@gmail.com>
2016-09-23 18:55:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
 //change text
-var element = getElementById("main-text");
+var element = document.getElementById("main-text");
 element.innerHTML = "This is the harry potter website!";
 
