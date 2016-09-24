08abcf4fce086da6dbb88d0d3af1be2726ee4d99
SiddharthHariprasad <siddhuhariprasad@gmail.com>
2016-09-24 19:24:05 +0530

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,27 @@ function moveRight () {
 img.onclick = function() {
 	var interval = setInterval(moveRight,50)
 }
+
+//counter code
+var button = document.getElementById('counter');
+
+button.onclick = function () {
+	
+	//create a req object
+	var request = new XMLHttpRequest();
+	
+	// capture response
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+	};
+	
+	//make a req
+	request.open('GET', 'http://siddharthhariprasad.imad.hasura-app.io/counter', true);
+	request.send(null);
+};
