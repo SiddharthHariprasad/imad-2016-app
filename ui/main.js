64ca929a19c41eb74d284abe7b96b991d6932fec
SiddharthHariprasad <siddhuhariprasad@gmail.com>
2016-09-24 20:32:19 +0530

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -41,3 +41,36 @@ button.onclick = function () {
 	request.open('GET', 'http://siddharthhariprasad.imad.hasura-app.io/counter', true);
 	request.send(null);
 };
+
+
+// Submit name
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+	
+	//create a req object
+	var request = new XMLHttpRequest();
+	
+	// capture response
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
+				//capture list and render as list
+				var names = request.responseText;
+				names = JSON.parse(names);
+				var list = '';
+				for (var i=0; i< names.length; i++) {
+					list += '<li>' + names[i] ;
+				}
+				var ul = document.getElementById('namelist');
+				ul.innerHTML = list;
+			}
+		}
+	};
+	//make a req and send name
+	var nameInput = document.getElementById('name');
+	var name = nameInput.value;
+	request.open('GET', 'http://siddharthhariprasad.imad.hasura-app.io/submit-name?=' + name, true);
+	request.send(null);
+
+	
+};
