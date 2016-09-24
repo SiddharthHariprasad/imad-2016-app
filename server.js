15d0252ddabd94ba40dfa70a2e4dc78ef5d76c89
SiddharthHariprasad <siddhuhariprasad@gmail.com>
2016-09-24 20:18:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -626,8 +626,6 @@ app.get('/counter',function (req, res){
     res.send(counter.toString());
 });
 
-
-
 app.get('/:articleName',function (req, res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
