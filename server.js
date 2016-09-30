7fee2deb23d6a62e5a7f1ac83187f04bd4844d74
SiddharthHariprasad <siddhuhariprasad@gmail.com>
2016-09-30 18:12:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -609,6 +609,13 @@ function createTemplate (data){
 					<div class = "head">
 						${controls}
 					</div>
+					<div class="comments">
+            			<input type="text" id="name" placeholder="Comment">
+            			<input type="submit" value="Submit" id="submit_btn">
+            			<ul id="namelist">
+            			</ul>
+            		</div>
+            	    <hr/>
                 </div>
             </body>
         </html>
