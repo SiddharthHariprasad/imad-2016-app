1ca0fc91e8680b0c0ed2ad40317a52083a46ff14
SiddharthHariprasad <siddhuhariprasad@gmail.com>
2016-10-28 21:30:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+  user: 'siddharthhariprasad',
+  database: 'siddharthhariprasad',
+  host: 'http://db.imad.hasura-app.io/',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
+
+
+var pool = new Pool(config);
+app.get('test-db', function (req, res) {
+    //make a select request
+    // return a response with results
+    pool.query('SELECT * FROM test', function (err, result){
+       if(err) {
+           res.status(500).send(err.toString());
+       } 
+       else {
+           res.send(JSON.stringify(result));
+       }
+    });
+});
+
 
 
 var app = express();
